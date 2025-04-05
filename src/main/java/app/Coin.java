b5a4c1540a5e90c8bfa6df4d13fea33ee0c88466
Sachin Gamhewa <107618727+SachinGamhewa@users.noreply.github.com>
2025-04-05 17:50:59 +0530

    Update Coin.java

--- src/main/java/app/Coin.java
+++ src/main/java/app/Coin.java
@@ -1,1 +1,26 @@
+package app;
 
+import java.util.Random;
+
+public class Coin {
+    private String side;
+    public String getSide(){return side;}
+    public void setSide (String side){this.side=side;}
+
+    private final String CONST_HEADS ="H";
+    public String getCONST_HEADS(){return CONST_HEADS;}
+
+    private final String CONST_TAILS ="T";
+    public String getCONST_TAILS(){return CONST_TAILS;}
+
+    public void flip(){
+        Random random =new Random();
+        int RandomVal = random.nextInt(2);
+        if (RandomVal==0) {
+            setSide(getCONST_HEADS());  
+        }
+        else{
+            setSide(getCONST_TAILS());
+        }
+    }   
+}
